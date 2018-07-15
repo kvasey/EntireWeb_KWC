19f18a18c5162c8c8387457af2fadcf0268a55a5
psimk <psimkevi@gmail.com>
2018-07-15 23:29:54 +0200

    A bunch of stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('KWC', () => App);
+import { AppRegistry } from "react-native";
+import App from "./src/App";
+
+AppRegistry.registerComponent("KWC", () => App);
