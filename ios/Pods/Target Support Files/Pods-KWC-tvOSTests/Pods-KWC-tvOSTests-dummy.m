a1a8b336acae8f8469dc31196f9073438c8cd1f1
gofenice <info@gofenice.com>
2018-11-21 02:46:10 +0530

    setup target.remove_from_project in ios

--- ios/Pods/Target Support Files/Pods-KWC-tvOSTests/Pods-KWC-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KWC_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_KWC_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-KWCTests/Pods-KWCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KWCTests : NSObject
-@end
-@implementation PodsDummy_Pods_KWCTests
-@end
