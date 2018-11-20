7d4b0557cc53a6579fd97b736e2907a1ffdd7079
gofenice <info@gofenice.com>
2018-11-20 23:58:02 +0530

    solved some errors in ios

---
+++ ios/Pods/Target Support Files/Pods-KWC/Pods-KWC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KWC : NSObject
+@end
+@implementation PodsDummy_Pods_KWC
+@end
---
+++ ios/Pods/Target Support Files/Pods-KWCTests/Pods-KWCTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KWCTests : NSObject
+@end
+@implementation PodsDummy_Pods_KWCTests
+@end
